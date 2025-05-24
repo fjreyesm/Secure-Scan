4782146a996ee1eb3769a58a069eab8d457b1f6d
fjreyesm <franklin.reyes.2003@gmail.com>
2025-05-24 12:15:36 +0200

    estilos

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"77foDpzLDtcxvYv24yZx5u0XnLDzZlt+caKRH2WOO6w=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"R3GiDlu2dJgYM6Z0cPlEBLt9bppKySHB2Vq/zZIOyws=\"\n}"
--- frontend/.next/static/chunks/app_layout_tsx_c0237562._.js
+++ frontend/.next/static/chunks/app_layout_tsx_c0237562._.js
@@ -1,11 +1,9 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_tailwindcss_dist_e99ea8d4._.js",
-  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
-  "static/chunks/node_modules_9da13448._.js",
-  "static/chunks/_e8d8aa34._.js",
-  "static/chunks/_a8f23bdd._.css"
+  "static/chunks/app_globals_73c37791.css",
+  "static/chunks/node_modules_f12cd717._.js",
+  "static/chunks/_e8d8aa34._.js"
 ],
     source: "dynamic"
 });
